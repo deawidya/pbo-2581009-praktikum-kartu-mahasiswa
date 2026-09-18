d16b392d8999beb7aa9e9a3571543d1b2c418f75
Dea Widya Ningsih <2581009@unai.edu>
2026-09-18 09:41:48 +0700

    Add input data Kartu Mahasiswa

--- src/KartuMahasiswa.java
+++ src/KartuMahasiswa.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 public class KartuMahasiswa {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan NIK: ");
         long nik = input.nextLong();
+
+        System.out.println("Masukkan Umur: ");
         int umur = input.nextInt();
+
+        System.out.println("Masukkan Tinggi Badan: ");
         double tinggiBadan = input.nextDouble();
+
+        System.out.println("Masukkan Golongan Darah: ");
         char golonganDarah = input.next().charAt(0);
+
+        System.out.println("Masukkan Status: ");
         boolean sudahMenikah = input.nextBoolean();
 
         input.nextLine();
